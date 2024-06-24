6e38eb4314af21e3f098daecc699803d51b34cac
Sarah Nichols <sarahnichols96@outlook.com>
2024-06-24 16:39:10 +0100

    add unzip

--- analysis/wgs-mining/00-build_kraken/01-download_pre-built_krakendb.sh
+++ analysis/wgs-mining/00-build_kraken/01-download_pre-built_krakendb.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-#SBATCH --job-name=build_kraken_db
+#SBATCH --job-name=download_kraken_capped
 #SBATCH --nodes=1
 #SBATCH --partition medium
 #SBATCH --mem=80G
+#SBATCH --clusters=ALL
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user sarah.nichols@biology.ox.ac.uk
 
@@ -14,5 +15,7 @@ set -eu
 source "/data/biol-bird-parasites/sann7416/wgs-mining-validation/src/.env"
 
 # .env file contains KRAKEN_CAPPED
-
+echo "Downloading to: $KRAKEN_CAPPED"
 wget https://genome-idx.s3.amazonaws.com/kraken/k2_pluspfp_16gb_20221209.tar.gz -O "$KRAKEN_CAPPED"
+
+tar -xvf "$KRAKEN_CAPPED" -C "$KRAKEN_CUSTOM"
