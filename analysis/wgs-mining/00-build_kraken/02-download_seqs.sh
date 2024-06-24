0fb8ef80de2946ccdd5265ec19ab697478017017
Sarah Nichols <sarahnichols96@outlook.com>
2024-06-24 16:39:55 +0100

    streamline db prep

--- analysis/wgs-mining/00-build_kraken/02-download_seqs.sh
+++ analysis/wgs-mining/00-build_kraken/02-download_seqs.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
-# download sequences from wormbase and eupath for building reference genome
+# download sequences from wormbase, eupath and ncbi for building reference genome
 
 #SBATCH --job-name=download_seqs
 #SBATCH --nodes=1
 #SBATCH --partition medium
 #SBATCH --mem=80G
+#SBATCH --clusters=all
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user sarah.nichols@biology.ox.ac.uk
 
 set -eu
-if [ -f "/data/zool-zost/sann7416/island-biogeography-wgs-mining/.env" ]; then
-   . "/data/zool-zost/sann7416/island-biogeography-wgs-mining/.env"
-fi
-# .env DOWNLOAD_SEQS
+source "/data/biol-bird-parasites/sann7416/wgs-mining-validation/src/.env"
+# .env DOWNLOAD_SEQS, NCBI_DATASETS_CONDA
 
 #wormbase
 wget -P "$DOWNLOAD_SEQS"/wormbase  -r -np -A "*.genomic.fa.gz" ftp://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/current/species/
+
+# Loop through each fasta file
+for file in "$DOWNLOAD_SEQS"/wormbase/**/*.fa.gz; 
+do
+echo "$file"
+  gunzip -k "$file"
+  decompressed_file="${file%.gz}"
+  echo "$decompressed_file"
+  # Get the name of the file without the extension
+  filename=$(basename "$decompressed_file" .fa)
+  echo "$filename"
+  bioproject=$(echo ${filename} | grep -oP "PRJ[A-Z]+[0-9]+")
+  # Add the file name to the header of each sequence in the file
+  sed -i "s/^>/>$bioproject|/" "$decompressed_file"
+done
+
+cat $"$DOWNLOAD_SEQS"/wormbase/**/*.fa >  $"$DOWNLOAD_SEQS"/wormbase/wormbase.fasta
+rm  $"$DOWNLOAD_SEQS"/wormbase/**/*.fa
 
 #eupath
 wget -P "$DOWNLOAD_SEQS"/eupath ftp://ftp.ccb.jhu.edu/pub/data/EuPathDB46/AmoebaDB46.tgz
@@ -29,3 +46,47 @@ wget -P "$DOWNLOAD_SEQS"/eupath ftp://ftp.ccb.jhu.edu/pub/data/EuPathDB46/Plasmo
 wget -P "$DOWNLOAD_SEQS"/eupath ftp://ftp.ccb.jhu.edu/pub/data/EuPathDB46/ToxoDB46.tgz
 wget -P "$DOWNLOAD_SEQS"/eupath ftp://ftp.ccb.jhu.edu/pub/data/EuPathDB46/TrichDB46.tgz
 wget -P "$DOWNLOAD_SEQS"/eupath ftp://ftp.ccb.jhu.edu/pub/data/EuPathDB46/TriTrypDB46.tgz
+
+for file in "$DOWNLOAD_SEQS"/eupath/*.tgz; do
+    tar -xvf "$file" -C "$DOWNLOAD_SEQS"/eupath
+done
+
+cat "$DOWNLOAD_SEQS"/eupath/**/*.fna > "$DOWNLOAD_SEQS"/eupath/eupath.fasta
+rm  $"$DOWNLOAD_SEQS"/eupath/**/*.fa
+
+module load Anaconda3/2024.02-1
+source activate $NCBI_DATASETS_CONDA
+
+datasets download genome taxon apicomplexa 
+unzip ncbi_dataset.zip -d "$DOWNLOAD_SEQS"/ncbi/apicomplexa
+find "$DOWNLOAD_SEQS"/ncbi/apicomplexa -type f -name "*.fna" -exec mv {} "$DOWNLOAD_SEQS"/ncbi/apicomplexa \;
+cat "$DOWNLOAD_SEQS"/ncbi/apicomplexa/*.fna > "$DOWNLOAD_SEQS"/ncbi/apicomplexa.fasta
+rm "$DOWNLOAD_SEQS"/ncbi/apicomplexa/*.fna
+
+datasets download genome taxon cercozoa
+unzip ncbi_dataset -d "$DOWNLOAD_SEQS"/ncbi/cercozoa
+find "$DOWNLOAD_SEQS"/ncbi/cercozoa -type f -name "*.fna" -exec mv {} "$DOWNLOAD_SEQS"/ncbi/cercozoa \;
+cat "$DOWNLOAD_SEQS"/ncbi/cercozoa/*.fna > "$DOWNLOAD_SEQS"/ncbi/cercozoa.fasta
+rm "$DOWNLOAD_SEQS"/ncbi/cercozoa/*.fna
+
+datasets download genome taxon euglenozoa
+unzip ncbi_dataset -d "$DOWNLOAD_SEQS"/ncbi/euglenozoa
+find "$DOWNLOAD_SEQS"/ncbi/euglenozoa -type f -name "*.fna" -exec mv {} "$DOWNLOAD_SEQS"/ncbi/euglenozoa \;
+cat "$DOWNLOAD_SEQS"/ncbi/euglenozoa/*.fna > "$DOWNLOAD_SEQS"/ncbi/euglenozoa.fasta
+rm "$DOWNLOAD_SEQS"/ncbi/euglenozoa/*.fna
+
+datasets download genome taxon fornicata 
+unzip ncbi_dataset -d "$DOWNLOAD_SEQS"/ncbi/fornicata
+find "$DOWNLOAD_SEQS"/ncbi/fornicata -type f -name "*.fna" -exec mv {} "$DOWNLOAD_SEQS"/ncbi/fornicata \;
+cat "$DOWNLOAD_SEQS"/ncbi/fornicata/*.fna > "$DOWNLOAD_SEQS"/ncbi/fornicata.fasta
+rm "$DOWNLOAD_SEQS"/ncbi/fornicata/*.fna
+
+datasets download genome taxon parabasalia 
+unzip ncbi_dataset -d "$DOWNLOAD_SEQS"/ncbi/parabasalia
+find "$DOWNLOAD_SEQS"/ncbi/parabasalia -type f -name "*.fna" -exec mv {} "$DOWNLOAD_SEQS"/ncbi/parabasalia \;
+cat "$DOWNLOAD_SEQS"/ncbi/parabasalia/*.fna > "$DOWNLOAD_SEQS"/ncbi/parabasalia.fasta
+rm "$DOWNLOAD_SEQS"/ncbi/parabasalia/*.fna
+
+conda deactivate $NCBI_DATASETS_CONDA
+
+
--- analysis/wgs-mining/00-build_kraken/03-add_seqs_kraken.sh
+++ analysis/wgs-mining/00-build_kraken/03-add_seqs_kraken.sh
@@ -9,13 +9,13 @@
 #SBATCH --mail-user sarah.nichols@biology.ox.ac.uk
 
 set -eu
-if [ -f "/data/zool-zost/sann7416/island-biogeography-wgs-mining/.env" ]; then
-   . "/data/zool-zost/sann7416/island-biogeography-wgs-mining/.env"
-fi
+source /data/biol-bird-parasites/sann7416/wgs-mining-validation/src/.env
 #env containing SEQS_TO_ADD and KRAKEN_CAPPED paths
 
 module purge
 module load Kraken2/2.1.2-gompi-2021b
+
+
 
 while IFS= read -r line;
 do  
