cf4002452e8c38cb8cb6b9353f38d5d0fbfe78b9
Sarah Nichols <sarahnichols96@outlook.com>
2024-07-04 14:45:07 +0100

    change file directory

--- analysis/metabarcoding/build_reference_db/crabs_download_seqs.sh
+++ analysis/metabarcoding/build_reference_db/crabs_download_seqs.sh
@@ -12,10 +12,23 @@ set -eu
 module load Anaconda3/2024.02-1
 
 # Activate Conda environment
-export CONPREFIX='/data/biol-bird-parasites/sann7416/conda_environments/CRABS_python3.8'
-conda activate $CONPREFIX
+export CONPREFIX='/data/zool-zost/sann7416/crabenv'
+source activate $CONPREFIX
 
-crabs db_download --source ncbi --database nucleotide --query '((haemosporidian[All Fields] NOT cytB[All Fields]) NOT cytochrome[All Fields]) NOT cox[All Fields] NOT ("Chordata"[Organism] OR Chordata[All Fields]) NOT "Nycteribia schmidlii"[porgn]' --output /data/biol-bird-parasites/sann7416/wgs-mining-validation/data/metabarcoding/ncbi/haemosporidians_nucleotide.fasta --keep_original yes --email sarahnichols96@outlook.com --batchsize 5000
-crabs db_download --source ncbi --database gene --query '"Haemosporida"[Organism] AND 18S[All Fields]' --output /data/biol-bird-parasites/sann7416/wgs-mining-validation/data/metabarcoding/ncbi/haemosporidians_gene.fasta --keep_original yes --email sarahnichols96@outlook.com --batchsize 5000
-crabs db_download --source ncbi --database genome --query '"Haemosporida"[Organism]' --output /data/biol-bird-parasites/sann7416/wgs-mining-validation/data/metabarcoding/ncbi/haemosporidians_genome.fasta --keep_original yes --email sarahnichols96@outlook.com --batchsize 5000
+cd /data/zool-zost/sann7416/reference_database_creator-main/
 
+./crabs db_download --source ncbi --database nucleotide --query '((haemosporidian[All Fields] NOT cytB[All Fields]) NOT cytochrome[All Fields]) NOT cox[All Fields] NOT ("Chordata"[Organism] OR Chordata[All Fields]) NOT "Nycteribia schmidlii"[porgn]' --output haemosporidians_nucleotide.fasta --keep_original yes --email sarahnichols96@outlook.com --batchsize 5000
+
+./crabs db_download --source taxonomy
+
+./crabs insilico_pcr --input haemosporidians_nucleotide.fasta --output haemosporidians_nucleotide_primers.fasta --fwd GCAAGTCTGGTGCCAG --rev CTTTAARTTTCASYCTTGCG --error 4.5
+
+./crabs assign_tax --input haemosporidians_nucleotide_primers.fasta --output haemosporidians_nucleotide_primers.tsv --acc2tax nucl_gb.accession2taxid --taxid nodes.dmp --name names.dmp --missing missing_taxa.tsv
+
+./crabs visualization --method diversity --input haemosporidians_nucleotide_primers.tsv --level species --output haemosporidians_nucleotide_primers_species_diversity.png
+
+./crabs tax_format --input haemosporidians_nucleotide_primers.tsv --output haemosporidian_references.fsa --format sintax
+./crabs tax_format --input haemosporidians_nucleotide_primers.tsv --output haemosporidian_references.fasta.gz --format idt
+
+cp haemosporidian_references.fasta.gz /data/biol-bird-parasites/sann7416/wgs-mining-validation/data/metabarcoding/reference_database/haemosporidian_reference.fasta.gz
+vsearch --sintax /data/biol-bird-parasites/sann7416/wgs-mining-validation/data/metabarcoding/ASVs.fasta --db haemosporidian_references.fsa --tabbedout vsearch_output.txt 
